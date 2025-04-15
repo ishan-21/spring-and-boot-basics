b23eb2c1e129bdebd222a4423caf1ce9d18aceac
ishan <raiishan0@gmail.com>
2025-04-15 18:45:00 +0530

    wrote meaningful comment for the rationale for non-removal of the app configuration class

--- src/main/java/com/ishan/configuration/AppConfiguration.java
+++ src/main/java/com/ishan/configuration/AppConfiguration.java
@@ -14,4 +14,16 @@ import org.springframework.stereotype.Component;
 @ComponentScan(basePackages = "com.ishan")
 public class AppConfiguration {
 
+    /*
+    Important Note:
+
+    > This class currently has almost no use as its only purpose is to
+    list down the two annotations: @Configuration and @ComponentScan. These
+    can be shifted to the main class (App.java) and the code will still work.
+
+    > Why I am not deleting this class => because it is best practice to create @Bean(s)
+    of third partu libraries and other classes that CANNOT annotated with @Component, for example
+    ObjectMapper, DataSource, etc. So this class is a placeholder for future use.
+     */
+    
 }
