90a5fa3bf71d93d097b9e21081c70e3bec0fd926
ishan <raiishan0@gmail.com>
2025-04-16 16:08:36 +0530

    using the Application class as the configuration class using @SpringbootApplication annotation now => deleted the previous configuration package

--- src/main/java/com/ishan/spring_boot_application/configuration/ApplicationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.ishan.spring_boot_application.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.ishan.spring_boot_application")
-public class ApplicationConfiguration {
-}
