4d6d674420a332351ca83b5bdc7ef1fa49871001
ishan <raiishan0@gmail.com>
2025-04-16 17:21:34 +0530

    changed the scope of the owner service bean to prototype

--- src/main/java/com/ishan/spring_boot_application/service/OwnerServiceImpl.java
+++ src/main/java/com/ishan/spring_boot_application/service/OwnerServiceImpl.java
@@ -2,9 +2,11 @@ package com.ishan.spring_boot_application.service;
 
 import com.ishan.spring_boot_application.repository.OwnerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 @Service
+@Scope("prototype") // this will create a new instance of the bean every time it is requested
 public class OwnerServiceImpl implements OwnerService {
 
     private final OwnerRepository ownerRepository;
