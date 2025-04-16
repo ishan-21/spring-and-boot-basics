427213d67696168fea4c5793a9bc2e4352dbb0cc
ishan <raiishan0@gmail.com>
2025-04-16 17:33:25 +0530

    wrote few lines of code to demonstrate the difference between the singleton and prototype scopes

--- src/main/java/com/ishan/spring_boot_application/Application.java
+++ src/main/java/com/ishan/spring_boot_application/Application.java
@@ -1,5 +1,6 @@
 package com.ishan.spring_boot_application;
 
+import com.ishan.spring_boot_application.repository.OwnerRepository;
 import com.ishan.spring_boot_application.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -20,7 +21,13 @@ public class Application implements CommandLineRunner {
 	// @SpringBootApplication also creates a bean of the Application class so things
 	// can be autowired here
 	@Autowired
-	OwnerService ownerService;
+	OwnerService ownerService1;
+	@Autowired
+	OwnerService ownerService2;
+	@Autowired
+	OwnerRepository ownerRepository1;
+	@Autowired
+	OwnerRepository ownerRepository2;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -31,7 +38,21 @@ public class Application implements CommandLineRunner {
 	// the run method here is internally called by Spring Boot inside the SpringApplication.run method
 	@Override
 	public void run(String... args) throws Exception {
-		String result = ownerService.findOwner(SAMPLE_OWNER_ID);
-		System.out.println(result);
+		String serviceHashCode1 = String.valueOf(ownerService1.hashCode());
+		String serviceHashCode2 = String.valueOf(ownerService2.hashCode());
+		String repositoryHashCode1 = String.valueOf(ownerRepository1.hashCode());
+		String repositoryHashCode2 = String.valueOf(ownerRepository2.hashCode());
+
+		if(serviceHashCode1.equalsIgnoreCase(serviceHashCode2)) {
+			System.out.println("OwnerService is of scope singleton");
+		} else {
+			System.out.println("OwnerService is of scope prototype");
+		}
+
+		if(repositoryHashCode1.equalsIgnoreCase(repositoryHashCode2)) {
+			System.out.println("OwnerRepository is of scope singleton");
+		} else {
+			System.out.println("OwnerRepository is of scope prototype");
+		}
 	}
 }
