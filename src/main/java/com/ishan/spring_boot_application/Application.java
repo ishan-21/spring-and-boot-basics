3095a8bc0bca7121bfad22d1c5ac7404f552ff6a
ishan <raiishan0@gmail.com>
2025-04-16 16:09:11 +0530

    using the Application class as the configuration class using @SpringbootApplication annotation now

--- src/main/java/com/ishan/spring_boot_application/Application.java
+++ src/main/java/com/ishan/spring_boot_application/Application.java
@@ -1,15 +1,16 @@
 package com.ishan.spring_boot_application;
 
-import com.ishan.spring_boot_application.configuration.ApplicationConfiguration;
 import com.ishan.spring_boot_application.service.OwnerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ishan.spring_boot_application")
+// @SpringBootApplication is a combination of @Configuration, @EnableAutoConfiguration, and @ComponentScan
 public class Application {
 
+	// now third party beans can be configured here itself using the @Bean annotation
 	public static final int SAMPLE_OWNER_ID = 1;
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
